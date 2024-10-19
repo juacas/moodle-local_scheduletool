9cf14ee2acab845a11ac2190bccc3ca3c6de148c
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2024-10-19 11:40:22 +0000

    fix namespace

--- settings.php
+++ settings.php
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+use local_scheduletool\admin_settings_coursecat_multiselect;
+use local_scheduletool\admin_setting_configmultiselect_autocomplete;
+
 defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/local/scheduletool/settingslib.php');
 
